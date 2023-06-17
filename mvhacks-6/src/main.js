989ba4afa578c8ff11b7f2332e51e54d5ec3efcf
DANIEL LIEF <90540132+TheMagicApple@users.noreply.github.com>
2023-06-17 08:48:33 -0700

    Initial Vue Setup

--- mvhacks-6/src/main.js
+++ mvhacks-6/src/main.js
@@ -1,4 +1,16 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
+import {createRouter,createWebHistory} from 'vue-router'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import Home from "./components/Home"
+
+const router=createRouter({
+	history:createWebHistory(),
+	routes:[
+	{path:"/",component:Home}
+	]
+})
+
+const app=createApp(App)
+app.use(router)
+app.mount('#app')
